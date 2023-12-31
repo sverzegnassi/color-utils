a88ba65b9c48609423b4723d8838aaf126c012de
Stefano Verzegnassi <stefano@sverzegnassi.me>
2023-12-31 06:41:25 +0100

    React-router-dom: get it working with GitHub Pages. Switch to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { BrowserRouter, Route, Routes, Outlet, Link as RouterLink, useLocation } from "react-router-dom";
+import { HashRouter, Route, Routes, Outlet, Link as RouterLink, useLocation } from "react-router-dom";
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 
 const ThemeGeneratorPage = lazy(() => import("./pages/ThemeGenerator.tsx"));
@@ -36,14 +36,14 @@ function PageLayout() {
 function App() {
   return (
     <>
-      <BrowserRouter future={{ v7_startTransition: true }}>
+      <HashRouter future={{ v7_startTransition: true }}>
         <Routes>
           <Route element={<PageLayout />}>
             <Route path="/" element={<ThemeGeneratorPage />} />
             <Route path="/random-neutral" element={<RandomNeutralColorPage />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
