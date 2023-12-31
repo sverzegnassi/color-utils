49361746c0db2cac68ab9eab7b06f904ab32d3be
Stefano Verzegnassi <stefano@sverzegnassi.me>
2023-12-31 06:14:51 +0100

    Added base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/color-utils/",
   plugins: [react()],
   server: {
     port: 3000,
